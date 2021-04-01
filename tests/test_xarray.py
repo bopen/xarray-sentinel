452cb2e69548ec2f59fe586b531794413fa76f0e
Alessandro Amici <a.amici@bopen.eu>
2021-04-02 00:09:59 +0200

    Return an empty dataset and pass the first integration test!

--- tests/test_xarray.py
+++ tests/test_xarray.py
@@ -3,3 +3,4 @@ import xarray as xr
 
 def test_open_dataset():
     ds = xr.open_dataset("dummy", engine="sentinel-1")
+    assert isinstance(ds, xr.Dataset)
--- xarray_sentinel/sentinel1.py
+++ xarray_sentinel/sentinel1.py
@@ -1,5 +1,9 @@
-import xarray
+import xarray as xr
 
 
-class Sentinel1Backend(xarray.backends.common.BackendEntrypoint):
-    pass
+class Sentinel1Backend(xr.backends.common.BackendEntrypoint):
+    def open_dataset(
+        self, filename_or_obj, *, drop_variables=None,
+    ):
+        ds = xr.Dataset()
+        return ds
