d4443bedd4d3634dcbb7c3518513c2602b049734
Alessandro Amici <a.amici@bopen.eu>
2021-04-02 13:47:58 +0200

    Weird way to silence the `Signature incompatible with supertype` mypy error

--- xarray_sentinel/sentinel1.py
+++ xarray_sentinel/sentinel1.py
@@ -4,11 +4,10 @@ import xarray as xr
 
 
 class Sentinel1Backend(xr.backends.common.BackendEntrypoint):
-    def open_dataset(
+    def open_dataset(   # type: ignore
         self,
         filename_or_obj: str,
         drop_variables: T.Optional[T.Tuple[str]] = None,
-        **kwargs: T.Any,
     ) -> xr.Dataset:
         ds = xr.Dataset()
         return ds
