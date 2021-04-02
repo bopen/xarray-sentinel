9856a5ea7f67e1546350cd12bf49af8a56aafefb
Alessandro Amici <a.amici@bopen.eu>
2021-04-02 13:41:17 +0200

    Fix type hints

--- tests/test_xarray.py
+++ tests/test_xarray.py
@@ -2,5 +2,5 @@ import xarray as xr
 
 
 def test_open_dataset() -> None:
-    ds = xr.open_dataset("dummy", engine="sentinel-1")
+    ds = xr.open_dataset("dummy", engine="sentinel-1")  # type: ignore
     assert isinstance(ds, xr.Dataset)
