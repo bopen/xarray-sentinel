366b50377a7f47bf343d938ada444893c34c4806
Alessandro Amici <a.amici@bopen.eu>
2021-04-02 13:52:15 +0200

    Code style

--- xarray_sentinel/sentinel1.py
+++ xarray_sentinel/sentinel1.py
@@ -4,10 +4,8 @@ import xarray as xr
 
 
 class Sentinel1Backend(xr.backends.common.BackendEntrypoint):
-    def open_dataset(   # type: ignore
-        self,
-        filename_or_obj: str,
-        drop_variables: T.Optional[T.Tuple[str]] = None,
+    def open_dataset(  # type: ignore
+        self, filename_or_obj: str, drop_variables: T.Optional[T.Tuple[str]] = None,
     ) -> xr.Dataset:
         ds = xr.Dataset()
         return ds
