dcbd367190bd58c3cd9e601ab0bfdea8a95adbe5
Alessandro Amici <a.amici@bopen.eu>
2021-04-05 11:45:41 +0200

    Use test data in open_dataset test

--- tests/test_xarray.py
+++ tests/test_xarray.py
@@ -1,6 +1,15 @@
+import pathlib
+
 import xarray as xr
+
+DATA_FOLDER = pathlib.Path(__file__).parent / "data"
 
 
 def test_open_dataset() -> None:
-    ds = xr.open_dataset("dummy", engine="sentinel-1")  # type: ignore
-    assert isinstance(ds, xr.Dataset)
+    product_path = (
+        DATA_FOLDER
+        / "S1B_IW_SLC__1SDV_20210401T052622_20210401T052650_026269_032297_EFA4.SAFE"
+    )
+    res = xr.open_dataset(product_path, engine="sentinel-1")  # type: ignore
+
+    assert isinstance(res, xr.Dataset)
