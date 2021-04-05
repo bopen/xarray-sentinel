29aac1143b01442ed51fc58ed8921135be531e6e
Alessandro Amici <a.amici@bopen.eu>
2021-04-06 01:00:36 +0200

    Add very rough implementation of open_gcp_dataset

--- tests/test_xarray.py
+++ tests/test_xarray.py
@@ -5,7 +5,7 @@ import xarray as xr
 DATA_FOLDER = pathlib.Path(__file__).parent / "data"
 
 
-def test_open_dataset() -> None:
+def test_open_dataset_root() -> None:
     product_path = (
         DATA_FOLDER
         / "S1B_IW_SLC__1SDV_20210401T052622_20210401T052650_026269_032297_EFA4.SAFE"
@@ -30,3 +30,18 @@ def test_open_dataset() -> None:
     res = xr.open_dataset(product_path)  # type: ignore
 
     assert isinstance(res, xr.Dataset)
+
+
+def test_open_dataset_gcp() -> None:
+    annotation_path = (
+        DATA_FOLDER
+        / "S1B_IW_SLC__1SDV_20210401T052622_20210401T052650_026269_032297_EFA4.SAFE"
+        / "annotation"
+        / "s1b-iw1-slc-vv-20210401t052624-20210401t052649-026269-032297-004.xml"
+    )
+    res = xr.open_dataset(annotation_path, engine="sentinel-1", group="gcp")
+
+    assert isinstance(res, xr.Dataset)
+    # assert set(res.dims) == {"time", "slant_range"}
+
+    print(res)
