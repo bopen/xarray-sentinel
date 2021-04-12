aa48474a3d97566ee56ff5151ca6a6fcaa67606d
Aureliana Barghini <a.barghini@bopen.eu>
2021-04-13 00:19:05 +0200

    add tests

--- tests/test_xarray.py
+++ tests/test_xarray.py
@@ -32,6 +32,32 @@ def test_open_dataset_root() -> None:
     assert isinstance(res, xr.Dataset)
 
 
+def test_open_dataset_orbit() -> None:
+    annotation_path = (
+        DATA_FOLDER
+        / "S1B_IW_SLC__1SDV_20210401T052622_20210401T052650_026269_032297_EFA4.SAFE"
+        / "annotation"
+        / "s1b-iw1-slc-vv-20210401t052624-20210401t052649-026269-032297-004.xml"
+    )
+    res = xr.open_dataset(annotation_path, engine="sentinel-1", group="orbit")  # type: ignore
+
+    assert isinstance(res, xr.Dataset)
+    assert set(res.dims) == {"time"}
+
+
+def test_open_dataset_attitude() -> None:
+    annotation_path = (
+        DATA_FOLDER
+        / "S1B_IW_SLC__1SDV_20210401T052622_20210401T052650_026269_032297_EFA4.SAFE"
+        / "annotation"
+        / "s1b-iw1-slc-vv-20210401t052624-20210401t052649-026269-032297-004.xml"
+    )
+    res = xr.open_dataset(annotation_path, engine="sentinel-1", group="attitude")  # type: ignore
+
+    assert isinstance(res, xr.Dataset)
+    assert set(res.dims) == {"time"}
+
+
 def test_open_dataset_gcp() -> None:
     annotation_path = (
         DATA_FOLDER
