048a5e258173f69d9ce42dd0f10bca4e825ad6f0
Corrado Avolio <corrado9999@gmail.com>
2021-04-18 16:05:49 +0200

    Add test for get_burst_info

--- tests/test_sentinel1.py
+++ tests/test_sentinel1.py
@@ -1,6 +1,8 @@
 import pathlib
 import tempfile
 
+import numpy as np
+
 from xarray_sentinel import sentinel1
 
 DATA_FOLDER = pathlib.Path(__file__).parent / "data"
@@ -29,3 +31,41 @@ def test_build_burst_id() -> None:
     burst_centre = [11.8475875, 47.16626783]
     burst_id = sentinel1.build_burst_id(product_attrs, burst_centre)
     assert burst_id == "R168-N118-E0472"
+
+
+def test_get_burst_info() -> None:
+    base_path = (
+        DATA_FOLDER
+        / "S1B_IW_SLC__1SDV_20210401T052622_20210401T052650_026269_032297_EFA4.SAFE"
+    )
+    subswath_data = {
+        "annotation_path": {
+            "vh": str(
+                base_path
+                / "annotation/s1b-iw1-slc-vh-20210401t052624-20210401t052649-026269-032297-001.xml"
+            ),
+        },
+    }
+    product_attrs = {"sat:relative_orbit": 168}
+    partial_expected_burst_info = {
+        "R168-N118-E0472": {
+            "burst_centre": np.array([11.8475875, 47.16626783]),
+            "burst_pos": 0,
+            "burst_first_line": 0,
+            "burst_last_line": 1500,
+            "burst_first_pixel": 0,
+            "burst_last_pixel": 21631,
+        },
+    }
+
+    burst_info = sentinel1.get_burst_info(product_attrs, subswath_data)
+    assert burst_info is not None
+
+    for burst_id in partial_expected_burst_info:
+        assert np.allclose(
+            np.asanyarray(burst_info[burst_id]["burst_centre"]),
+            np.asanyarray(partial_expected_burst_info[burst_id]["burst_centre"]),
+        )
+        del burst_info[burst_id]["burst_centre"]
+        del partial_expected_burst_info[burst_id]["burst_centre"]
+        assert burst_info[burst_id] == partial_expected_burst_info[burst_id]
