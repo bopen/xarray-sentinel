70fae4fea96403adb5b5fae043bf43356911ac96
Corrado Avolio <corrado9999@gmail.com>
2021-04-18 15:58:53 +0200

    Add test for opening a burst
    Currently marked as xfail because test data files are not present on
    repository.

--- tests/test_xarray.py
+++ tests/test_xarray.py
@@ -1,5 +1,6 @@
 import pathlib
 
+import pytest
 import xarray as xr
 
 DATA_FOLDER = pathlib.Path(__file__).parent / "data"
@@ -74,3 +75,16 @@ def test_open_subswath() -> None:
 
     assert isinstance(res, xr.Dataset)
     assert not res.variables
+
+
+@pytest.mark.xfail
+def test_open_burst() -> None:
+    product_path = (
+        DATA_FOLDER
+        / "S1B_IW_SLC__1SDV_20210401T052622_20210401T052650_026269_032297_EFA4.SAFE"
+    )
+    res = xr.open_dataset(product_path, engine="sentinel-1", group="IW1/R168-N118-E0472")  # type: ignore
+
+    assert isinstance(res, xr.Dataset)
+    assert res.dims == {"x": 21632, "y": 1501}
+    assert set(res.variables) == {"VH", "VV", "x", "y"}
