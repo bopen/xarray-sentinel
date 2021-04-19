f979104e55f3a23ba7575f172f7f6c2339f050fa
Alessandro Amici <a.amici@bopen.eu>
2021-04-20 00:00:40 +0200

    Add a few considerations about time accuracy

--- xarray_sentinel/conventions.py
+++ xarray_sentinel/conventions.py
@@ -3,7 +3,7 @@ import xarray as xr
 VARIABLE_ATTRIBUTES = {
     "azimuth_time": {"long_name": "azimuth time", "standard_name": "time"},
     # NOTE: `slant_range_time` is not expressed as `np.timedelta64[ns]` in order to keep enough
-    #   accuracy for interferometric processing, i.e. c * 1ns ~= 30cm.
+    #   accuracy for interferometric processing, i.e. c * 1ns / 2 ~= 15cm.
     "slant_range_time": {"units": "s", "long_name": "slant range time / two-way delay"},
     "latitude": {"units": "degrees_north"},
     "longitude": {"units": "degrees_east"},
