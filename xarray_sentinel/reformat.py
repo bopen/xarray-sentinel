7b8c7897202d5632d4c09adf923bc18cc9f59acc
Alessandro Amici <a.amici@bopen.eu>
2022-01-04 19:07:04 +0100

    Fix reformat

--- xarray_sentinel/reformat.py
+++ xarray_sentinel/reformat.py
@@ -9,6 +9,6 @@ def to_group_zarr(product_path: esa_safe.PathType, output_store: T.Any) -> None:
     root = xr.open_dataset(product_path, engine="sentinel-1")  # type: ignore
     root.to_zarr(output_store, mode="w")
 
-    for group in root.attrs["groups"]:
+    for group in root.attrs["subgroups"]:
         group_ds = xr.open_dataset(product_path, engine="sentinel-1", group=group)  # type: ignore
         group_ds.to_zarr(output_store, mode="a", group=group)
