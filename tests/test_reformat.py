43e7ae229fe9e108d4716b9b89a3d6d633e721b2
Alessandro Amici <a.amici@bopen.eu>
2022-01-04 20:19:11 +0100

    Add non-slow tests for reformat.py

---
+++ tests/test_reformat.py
@@ -0,0 +1,31 @@
+import pathlib
+import typing as T
+
+import pytest
+
+from xarray_sentinel import reformat
+
+DATA_FOLDER = pathlib.Path(__file__).parent / "data"
+
+
+def test_to_group_zarr(tmpdir: T.Any) -> None:
+    product_path = (
+        DATA_FOLDER
+        / "S1B_IW_SLC__1SDV_20210401T052622_20210401T052650_026269_032297_EFA4.SAFE"
+    )
+    tmp_path = str(tmpdir.join("tmp.zarr"))
+    groups = {"IW1/VV/gcp": "IW1/VV/gcp", "IW2/VH/attitude": "IW2/VH/attitude"}
+
+    reformat.to_group_zarr(product_path, tmp_path, groups)
+
+
+@pytest.mark.xfail
+def test_to_group_netcdf(tmpdir: T.Any) -> None:
+    product_path = (
+        DATA_FOLDER
+        / "S1B_IW_SLC__1SDV_20210401T052622_20210401T052650_026269_032297_EFA4.SAFE"
+    )
+    tmp_path = str(tmpdir.join("tmp.nc"))
+    groups = {"IW1/VV/gcp": "IW1/VV/gcp", "IW2/VH/attitude": "IW2/VH/attitude"}
+
+    reformat.to_group_netcdf(product_path, tmp_path, groups)
--- xarray_sentinel/reformat.py
+++ xarray_sentinel/reformat.py
@@ -5,23 +5,36 @@ import xarray as xr
 from . import esa_safe
 
 
-def to_group_zarr(product_path: esa_safe.PathType, output_store: T.Any) -> None:
+def to_group_zarr(
+    product_path: esa_safe.PathType,
+    output_store: T.Any,
+    groups: T.Optional[T.Dict[str, str]] = None,
+) -> None:
     root = xr.open_dataset(product_path, engine="sentinel-1")  # type: ignore
     root.to_zarr(output_store, mode="w")
 
-    for group in root.attrs["subgroups"]:
-        group_ds = xr.open_dataset(product_path, engine="sentinel-1", group=group)  # type: ignore
-        group_ds.to_zarr(output_store, mode="a", group=group)
+    if groups is None:
+        groups = {g: g for g in root.attrs["subgroups"]}
+
+    for group_out, group_in in groups.items():
+        group_ds = xr.open_dataset(product_path, engine="sentinel-1", group=group_in)  # type: ignore
+        group_ds.to_zarr(output_store, mode="a", group=group_out)
 
 
 # Apparently there is no way to save SLC images because "netcdf4" doesn't support complex data
 # and "n5necdf" crashes on same name dimensions.
 def to_group_netcdf(
-    product_path: esa_safe.PathType, output_store: str, engine: T.Optional[str] = None
+    product_path: esa_safe.PathType,
+    output_store: str,
+    groups: T.Optional[T.Dict[str, str]] = None,
+    engine: T.Optional[str] = None,
 ) -> None:
     root = xr.open_dataset(product_path, engine="sentinel-1")  # type: ignore
     root.to_netcdf(output_store, mode="w", engine=engine)
 
-    for group in root.attrs["subgroups"]:
-        group_ds = xr.open_dataset(product_path, engine="sentinel-1", group=group)  # type: ignore
-        group_ds.to_netcdf(output_store, mode="a", group=group, engine=engine)
+    if groups is None:
+        groups = {g: g for g in root.attrs["subgroups"]}
+
+    for group_out, group_in in groups.items():
+        group_ds = xr.open_dataset(product_path, engine="sentinel-1", group=group_in)  # type: ignore
+        group_ds.to_netcdf(output_store, mode="a", group=group_out, engine=engine)
