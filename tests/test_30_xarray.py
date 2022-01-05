c77f284e7f7c71eb3116256e9965a50ee4d817e0
Alessandro Amici <a.amici@bopen.eu>
2022-01-05 12:17:28 +0100

    Remove more unused tests

--- tests/test_30_xarray.py
+++ tests/test_30_xarray.py
@@ -1,6 +1,5 @@
 import pathlib
 
-import numpy as np
 import pytest
 import xarray as xr
 
@@ -146,57 +145,6 @@ def test_open_pol_dataset() -> None:
 
     assert set(res.dims) == {"line", "pixel"}
     assert set(res.variables) == expected_variables
-
-
-@pytest.mark.skip
-def test_open_burst() -> None:
-    product_path = (
-        DATA_FOLDER
-        / "S1B_IW_SLC__1SDV_20210401T052622_20210401T052650_026269_032297_EFA4.SAFE"
-    )
-    res = xr.open_dataset(product_path, engine="sentinel-1", group="IW1/R168-N471-E0118")  # type: ignore
-
-    assert isinstance(res, xr.Dataset)
-    for attr_name in COMMON_ATTRIBUTES:
-        assert attr_name in res.attrs
-        assert res.attrs[attr_name] == COMMON_ATTRIBUTES[attr_name]
-    assert res.dims == {"slant_range_time": 21632, "azimuth_time": 1501}
-    assert not np.all(np.isnan(res.VH))
-    assert not np.all(np.isnan(res.VH))
-
-    expected = {
-        "VH",
-        "VV",
-        "slant_range_time",
-        "azimuth_time",
-        "line",
-        "pixel",
-    }
-    assert set(res.variables) == expected
-
-
-@pytest.mark.skip
-def test_open_burst_one_pol() -> None:
-    product_path = (
-        DATA_FOLDER
-        / "S1B_IW_SLC__1SDV_20210401T052622_20210401T052650_026269_032297_EFA4.SAFE"
-    )
-    res = xr.open_dataset(product_path, engine="sentinel-1", group="IW2/R168-N473-E0107")  # type: ignore
-
-    assert isinstance(res, xr.Dataset)
-    for attr_name in COMMON_ATTRIBUTES:
-        assert attr_name in res.attrs
-        assert res.attrs[attr_name] == COMMON_ATTRIBUTES[attr_name]
-    assert res.dims == {"slant_range_time": 25508, "azimuth_time": 1513}
-
-    expected = {
-        "VH",
-        "slant_range_time",
-        "azimuth_time",
-        "line",
-        "pixel",
-    }
-    assert set(res.variables) == expected
 
 
 def test_open_calibration_dataset() -> None:
