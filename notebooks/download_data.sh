63d6ffda28279b6412a5ae6d6594bb5555fa4b90
Alessandro Amici <a.amici@bopen.eu>
2022-01-22 18:28:48 +0100

    Download GRD images as well

--- notebooks/download_data.sh
+++ notebooks/download_data.sh
@@ -1,10 +1,22 @@
 
 mkdir -p data
 
-# SLC SM
+# SM
+
+## SLC
 sentinelsat --path data -d --include-pattern "*vv*" \
   --name S1B_S6_SLC__1SDV_20211216T115438_20211216T115501_030050_03968A_4DCB
 
-# SLC IW
+## GRD
+sentinelsat --path data -d --include-pattern "*vv*" \
+  --name S1B_S6_GRDH_1SDV_20211216T115438_20211216T115501_030050_03968A_0F8A
+
+# IW
+
+## SLC
 sentinelsat --path data -d --include-pattern "*iw3*vv*" \
   --name S1B_IW_SLC__1SDV_20211223T051121_20211223T051148_030148_039993_BA4B
+
+## GRD
+sentinelsat --path data -d --include-pattern "*vv*" \
+  --name S1B_IW_GRDH_1SDV_20211223T051122_20211223T051147_030148_039993_5371
