08b33bc7818184537284e66f4d6b45bc3195cd23
Alessandro Amici <a.amici@bopen.eu>
2022-07-24 18:32:45 +0200

    Update xarray backend to accept new arguments

--- xarray_sentinel/xarray_backends.py
+++ xarray_sentinel/xarray_backends.py
@@ -1,6 +1,7 @@
 import os
 from typing import Any, Dict, Optional, Tuple
 
+import fsspec
 import xarray as xr
 
 from . import sentinel1
@@ -14,6 +15,9 @@ class Sentinel1Backend(xr.backends.common.BackendEntrypoint):
         group: Optional[str] = None,
         storage_options: Optional[Dict[str, Any]] = None,
         override_product_files: Optional[str] = None,
+        fs: Optional[fsspec.AbstractFileSystem] = None,
+        check_files_exist: bool = False,
+        parse_geospatial_attrs: bool = True,
     ) -> xr.Dataset:
         ds = sentinel1.open_sentinel1_dataset(
             filename_or_obj,
@@ -21,6 +25,9 @@ class Sentinel1Backend(xr.backends.common.BackendEntrypoint):
             group=group,
             storage_options=storage_options,
             override_product_files=override_product_files,
+            fs=fs,
+            check_files_exist=check_files_exist,
+            parse_geospatial_attrs=parse_geospatial_attrs,
         )
         return ds
 
