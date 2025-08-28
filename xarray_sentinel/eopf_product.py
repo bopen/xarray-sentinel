c7252da70dcaf4305c2952614dcf7b28ed2784e9
Alessandro Amici <a.amici@bopen.eu>
2025-08-28 11:31:13 +0200

    mount the groups in the correct place

--- xarray_sentinel/eopf_product.py
+++ xarray_sentinel/eopf_product.py
@@ -1,10 +1,11 @@
 import logging
+import pathlib
 from typing import Any
 
 import fsspec
 import xarray as xr
 
-from . import esa_safe, sentinel1
+from . import eopf_metadata, esa_safe, sentinel1
 
 
 def open_datatree(
@@ -14,41 +15,82 @@ def open_datatree(
     storage_options: dict[str, Any] | None = None,
     check_files_exist: bool = False,
     override_product_files: str | None = None,
-    parse_geospatial_attrs: bool = True,
     **kwargs: Any,
 ) -> xr.DataTree:
+    product_name = pathlib.Path(product_urlpath).stem
     root = sentinel1.open_sentinel1_dataset(
         product_urlpath,
         fs=fs,
         storage_options=storage_options,
         check_files_exist=check_files_exist,
+        override_product_files=override_product_files,
     )
+    xarray_sentinel_groups = root.attrs["subgroups"]
     dt = xr.DataTree()
-    for group in root.attrs["subgroups"]:
-        subgroup = group.partition("/")[2]
+    for xarray_sentinel_group in xarray_sentinel_groups:
+        _, _, pol_group = xarray_sentinel_group.partition("/")
         try:
-            product_type, _, dataset = subgroup.partition("/")
-            if not product_type:
+            pol, _, dataset = pol_group.partition("/")
+            eopf_product_name = f"{product_name}_{pol.upper()}"
+            if not pol:
                 continue
             if not dataset:
-                dt[f"{product_type}/measurement"] = sentinel1.open_sentinel1_dataset(
+                if eopf_product_name not in dt.children:
+                    product_ds = xr.Dataset(
+                        attrs={
+                            "other_metadata": eopf_metadata.build_other_metadata(
+                                product_urlpath
+                            ),
+                            "stac_metadata": {},
+                        }
+                    )
+                    dt[f"{eopf_product_name}"] = product_ds
+                ds = sentinel1.open_sentinel1_dataset(
                     product_urlpath,
                     fs=fs,
                     storage_options=storage_options,
                     check_files_exist=check_files_exist,
-                    group=group,
+                    override_product_files=override_product_files,
+                    group=xarray_sentinel_group,
                 )
+                ds.attrs.clear()
+                dt[f"{eopf_product_name}/measurement"] = ds.rename(
+                    {"measurement": "slc"}
+                )
+            elif dataset in {
+                "antenna_pattern",
+                "orbit",
+                "attitude",
+                "azimuth_fm_rate",
+                "doppler_centroid",
+                "reference_replica",
+                "replica",
+                "gcp",
+            }:
+                ds = sentinel1.open_sentinel1_dataset(
+                    product_urlpath,
+                    fs=fs,
+                    storage_options=storage_options,
+                    check_files_exist=check_files_exist,
+                    group=xarray_sentinel_group,
+                    override_product_files=override_product_files,
+                )
+                ds.attrs.clear()
+                dt[f"{eopf_product_name}/conditions/{dataset}"] = ds
+            elif dataset in {"calibration", "noise_range", "noise_azimuth"}:
+                ds = sentinel1.open_sentinel1_dataset(
+                    product_urlpath,
+                    fs=fs,
+                    storage_options=storage_options,
+                    check_files_exist=check_files_exist,
+                    group=xarray_sentinel_group,
+                    override_product_files=override_product_files,
+                )
+                ds.attrs.clear()
+                dt[f"{eopf_product_name}/quality/{dataset}"] = ds
             else:
-                dt[f"{product_type}/conditions/{dataset}"] = (
-                    sentinel1.open_sentinel1_dataset(
-                        product_urlpath,
-                        fs=fs,
-                        storage_options=storage_options,
-                        check_files_exist=check_files_exist,
-                        group=group,
-                    )
-                )
+                print(f"Skipping {xarray_sentinel_group=}")
         except Exception:
-            logging.exception(f"{group=} failed to load")
+            logging.exception(f"{xarray_sentinel_group=} failed to load")
 
     return dt
