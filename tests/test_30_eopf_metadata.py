101cd70c16ef01151e12fcce97901d8ecb1b969c
Alessandro Amici <a.amici@bopen.eu>
2025-08-28 20:06:55 +0200

    Fix tests names and values

--- tests/test_30_eopf_metadata.py
+++ tests/test_30_eopf_metadata.py
@@ -4,14 +4,14 @@ from xarray_sentinel import eopf_metadata
 
 DATA_FOLDER = pathlib.Path(__file__).parent / "data"
 
-SLC_IW = (
+SLC_S3 = (
     DATA_FOLDER
-    / "S1B_IW_SLC__1SDV_20210401T052622_20210401T052650_026269_032297_EFA4.SAFE"
+    / "S1A_S3_SLC__1SDV_20210401T152855_20210401T152914_037258_04638E_6001.SAFE"
 )
-SLC_IW1_VV_annotation = (
-    SLC_IW
+SLC_S3_VH_annotation = (
+    SLC_S3
     / "annotation"
-    / "s1b-iw1-slc-vv-20210401t052624-20210401t052649-026269-032297-004.xml"
+    / "s1a-s3-slc-vh-20210401t152855-20210401t152914-037258-04638e-001.xml"
 )
 
 
@@ -25,18 +25,18 @@ def test_to_snake_recursive() -> None:
 
 
 def test_build_other_metadata() -> None:
-    res = eopf_metadata.build_other_metadata(SLC_IW1_VV_annotation)
+    res = eopf_metadata.build_other_metadata(SLC_S3_VH_annotation)
     expected_quality_information = {
         "product_quality_index": 0.0,
         "quality_data_list": [
             {
-                "azimuth_time": "2021-04-01T05:26:24.209990",
+                "azimuth_time": "2021-04-01T15:28:55.111501",
                 "downlink_quality": {
-                    "i_input_data_mean": 0.3338871002197266,
-                    "q_input_data_mean": 0.08810008317232132,
+                    "i_input_data_mean": 0.06302300840616226,
+                    "q_input_data_mean": 0.114902101457119,
                     "input_data_mean_outside_nominal_range_flag": False,
-                    "i_input_data_std_dev": 10.48145961761475,
-                    "q_input_data_std_dev": 10.50522994995117,
+                    "i_input_data_std_dev": 1.674363970756531,
+                    "q_input_data_std_dev": 1.515843987464905,
                     "input_data_st_dev_outside_nominal_range_flag": False,
                     "num_downlink_input_data_gaps": 0,
                     "downlink_gaps_in_input_data_significant_flag": False,
@@ -54,21 +54,21 @@ def test_build_other_metadata() -> None:
                     "pg_source_used": "Extracted",
                     "rrf_spectrum_used": "Extended Tapered",
                     "replica_reconstruction_failed_flag": False,
-                    "mean_pg_product_amplitude": 0.624697744846344,
-                    "std_dev_pg_product_amplitude": 0.006798196118324995,
-                    "mean_pg_product_phase": 0.9386667609214783,
-                    "std_dev_pg_product_phase": 0.01930363848805428,
+                    "mean_pg_product_amplitude": 0.8810305595397949,
+                    "std_dev_pg_product_amplitude": 0.006521929986774921,
+                    "mean_pg_product_phase": 0.07730674743652344,
+                    "std_dev_pg_product_phase": 0.01562887243926525,
                     "pg_product_derivation_failed_flag": False,
                     "invalid_downlink_params_flag": True,
                 },
                 "raw_data_analysis_quality": {
-                    "i_bias": 0.3338871002197266,
+                    "i_bias": 0.06302300840616226,
                     "i_bias_significance_flag": True,
-                    "q_bias": 0.08810008317232132,
+                    "q_bias": 0.114902101457119,
                     "q_bias_significance_flag": True,
-                    "iq_gain_imbalance": 0.9977372884750366,
+                    "iq_gain_imbalance": 1.104575037956238,
                     "iq_gain_significance_flag": True,
-                    "iq_quadrature_departure": 0.04520197957754135,
+                    "iq_quadrature_departure": -0.5162643194198608,
                     "iq_quadrature_departure_significance_flag": True,
                 },
                 "doppler_centroid_quality": {
@@ -77,8 +77,8 @@ def test_build_other_metadata() -> None:
                 },
                 "image_quality": {
                     "image_statistics": {
-                        "output_data_mean": {"re": -0.01723112, "im": 0.01067772},
-                        "output_data_std_dev": {"re": 85.68334, "im": 85.67197},
+                        "output_data_mean": {"re": -0.02062067, "im": 0.01445807},
+                        "output_data_std_dev": {"re": 6.993358, "im": 6.996273},
                     },
                     "output_data_mean_outside_nominal_range_flag": True,
                     "output_data_st_dev_outside_nominal_range_flag": True,
@@ -86,6 +86,7 @@ def test_build_other_metadata() -> None:
             }
         ],
     }
+
     assert len(res) == 5
 
     assert res["quality_information"] == expected_quality_information
--- tests/test_40_eopf_product.py
+++ tests/test_40_eopf_product.py
@@ -6,13 +6,13 @@ from xarray_sentinel import eopf_product
 
 DATA_FOLDER = pathlib.Path(__file__).parent / "data"
 
-SLC_IW = (
+SLC_S3 = (
     DATA_FOLDER
     / "S1A_S3_SLC__1SDV_20210401T152855_20210401T152914_037258_04638E_6001.SAFE"
 )
 
 
 def test_open_datatree() -> None:
-    res = eopf_product.open_datatree(SLC_IW)
+    res = eopf_product.open_datatree(SLC_S3)
 
     assert isinstance(res, xr.DataTree)
