54125534fc06903e4027fd9eeec82337a2d40f80
Alessandro Amici <a.amici@bopen.eu>
2025-08-28 11:44:14 +0200

    Code style

--- tests/test_40_eopf_product.py
+++ tests/test_40_eopf_product.py
@@ -1,6 +1,7 @@
 import pathlib
 
 import xarray as xr
+
 from xarray_sentinel import eopf_product
 
 DATA_FOLDER = pathlib.Path(__file__).parent / "data"
@@ -9,6 +10,7 @@ SLC_IW = (
     DATA_FOLDER
     / "S1B_IW_SLC__1SDV_20210401T052622_20210401T052650_026269_032297_EFA4.SAFE"
 )
+
 
 def test_open_datatree() -> None:
     res = eopf_product.open_datatree(SLC_IW)
