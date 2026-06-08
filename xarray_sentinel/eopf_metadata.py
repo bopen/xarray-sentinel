c26a49efcb82b1e20f70b90491bb6426182d57c2
Alessandro Amici <a.amici@bopen.eu>
2026-06-08 09:58:52 +0200

    Vendor the to_snake function from pydantic

--- xarray_sentinel/eopf_metadata.py
+++ xarray_sentinel/eopf_metadata.py
@@ -1,21 +1,44 @@
+import re
 import warnings
 from typing import Any
 
 import fsspec
 import numpy as np
-import pydantic.alias_generators
 
 from . import esa_safe
+
+
+# taken from pydantic.alias_generators, MIT license
+def to_snake(camel: str) -> str:
+    """Convert a PascalCase, camelCase, or kebab-case string to snake_case.
+
+    Args:
+        camel: The string to convert.
+
+    Returns
+    -------
+        The converted string in snake_case.
+    """
+    # Handle the sequence of uppercase letters followed by a lowercase letter
+    snake = re.sub(
+        r"([A-Z]+)([A-Z][a-z])", lambda m: f"{m.group(1)}_{m.group(2)}", camel
+    )
+    # Insert an underscore between a lowercase letter and an uppercase letter
+    snake = re.sub(r"([a-z])([A-Z])", lambda m: f"{m.group(1)}_{m.group(2)}", snake)
+    # Insert an underscore between a digit and an uppercase letter
+    snake = re.sub(r"([0-9])([A-Z])", lambda m: f"{m.group(1)}_{m.group(2)}", snake)
+    # Insert an underscore between a lowercase letter and a digit
+    snake = re.sub(r"([a-z])([0-9])", lambda m: f"{m.group(1)}_{m.group(2)}", snake)
+    # Replace hyphens with underscores to handle kebab-case
+    snake = snake.replace("-", "_")
+    return snake.lower()
 
 
 def to_snake_recursive(
     struct: dict[str, Any] | list[Any],
 ) -> dict[str, Any] | list[Any]:
     if isinstance(struct, dict):
-        struct = {
-            pydantic.alias_generators.to_snake(k): to_snake_recursive(v)
-            for k, v in struct.items()
-        }
+        struct = {to_snake(k): to_snake_recursive(v) for k, v in struct.items()}
     elif isinstance(struct, list):
         struct = [to_snake_recursive(v) for v in struct]
     return struct
